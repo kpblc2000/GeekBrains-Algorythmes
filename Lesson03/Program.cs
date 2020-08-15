d3f1b6e7c7b16f5be31f109242f529f0b0541c64
Alexey Kulik <kpblc2000@gmail.com>
2020-08-15 08:10:19 +0300

    Добавление меню для вызова. Использование клонирования массивов.

--- Lesson03/Program.cs
+++ Lesson03/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
+
+/// <summary>
+/// Алексей Кулик kpblc2000@yandex.ru
+/// Алгоритмы, урок 3
+/// </summary>
 
 namespace Lesson03
 {
@@ -8,99 +14,225 @@ namespace Lesson03
 
 		static void Main()
 		{
-			// Заполнение массивов
-
-			long counter = 0;
-			int v1 = 10, v2 = 20;
-			Console.WriteLine($"{v1} {v2} {counter}");
-			Swap(ref v1, ref v2, ref counter);
-			Console.WriteLine($"{v1} {v2} {counter}");
 
 			int[] ar;
 
+			long swapCounter, cycleCounter;
 
-			//int[][] ar1 = new int[3][];
-			//ar1[0] = CInitArrays.Array100;
-			//ar1[1] = CInitArrays.Array500;
-			//ar1[2] = CInitArrays.Array1000;
+			Console.WriteLine(@"Выберите один из вариантов массивов для сравнения количества выполняемых операций:
+1. 10 элементов
+2. 100 элементов
+3. 500 элементов
+4. 1000 элементов");
+			int answer;
+			int.TryParse(Console.ReadLine(), out answer);
 
-			counter = 0;
-			ar = CInitArrays.Array100;
-			BubbleSort(ar, ref counter);
-			Console.WriteLine($"Bubble swaps {ar.Length}: {counter}");
-			counter = 0;
-			ar = CInitArrays.Array500;
-			BubbleSort(ar, ref counter);
-			Console.WriteLine($"Bubble swaps {ar.Length}: {counter}");
-			ar = CInitArrays.Array1000;
-			BubbleSort(ar, ref counter);
-			Console.WriteLine($"Bubble swaps {ar.Length}: {counter}");
+			switch (answer)
+			{
+				case 1:
+					ar = CInitArrays.Array10;
+					break;
+				case 2:
+					ar = CInitArrays.Array100;
+					break;
+				case 3:
+					ar = CInitArrays.Array500;
+					break;
+				case 4:
+					ar = CInitArrays.Array1000;
+					break;
+				default:
+					return;
+			}
 
-			counter = 0;
-			ar = CInitArrays.Array100;
-			ShakerSort(ar, ref counter);
-			Console.WriteLine($"Shaker swaps {ar.Length}: {counter}");
-			counter = 0;
-			ar = CInitArrays.Array500;
-			ShakerSort(ar, ref counter);
-			Console.WriteLine($"Shaker swaps {ar.Length}: {counter}");
-			ar = CInitArrays.Array1000;
-			ShakerSort(ar, ref counter);
-			Console.WriteLine($"Shaker swaps {ar.Length}: {counter}");
+			Console.WriteLine($"\nBase array {ar.Length}: ");
+			ArrayToConsole(ar);
+			BubbleSortSimple(ar, out swapCounter, out cycleCounter);
+			Console.WriteLine($"BubbleSortSimple -> Swaps: {swapCounter}; Cycles: {cycleCounter}");
 
+			BubbleSort(ar, out swapCounter, out cycleCounter);
+			Console.WriteLine($"BubbleSort       -> Swaps: {swapCounter}; Cycles: {cycleCounter}");
+
+			ShakerSort1(ar, out swapCounter, out cycleCounter);
+			Console.WriteLine($"ShakerSort1      -> Swaps: {swapCounter}; Cycles: {cycleCounter}");
+
+			ShakerSort2(ar, out swapCounter, out cycleCounter);
+			Console.WriteLine($"ShakerSort2      -> Swaps: {swapCounter}; Cycles: {cycleCounter}");
+
+			InsSort1(ar, out swapCounter, out cycleCounter);
+			Console.WriteLine($"InsSort1         -> Swaps: {swapCounter}; Cycles: {cycleCounter}");
+
+			Console.WriteLine("Нажмите любую клавишу...");
 			Console.ReadKey();
 		}
 
+		/// <summary>
+		/// Простой вывод [части] массива в консоль. Выводится 10 первых элементов массива либо весь массив, если его размер меньше 10
+		/// </summary>
+		/// <param name="ArrayToPrint">Обрабатываемый массив</param>
+		public static void ArrayToConsole(int[] ArrayToPrint)
+		{
+			for (int i = 0; i < (ArrayToPrint.Length > 10 ? 10 : ArrayToPrint.Length); i++)
+			{
+				Console.Write($"{ArrayToPrint[i]}\t");
+			}
+			Console.WriteLine(ArrayToPrint.Length > 10 ? "..." : "");
+		}
+
+		/// <summary>
+		/// Замена местами двух переменных.
+		/// </summary>
+		/// <param name="Value1">Переменная 1</param>
+		/// <param name="Value2">Переменная 2</param>
+		/// <param name="Counter">Счетчик замен (используется только для тестовых задач</param>
 		public static void Swap(ref int Value1, ref int Value2, ref long Counter)
 		{
 			Counter++;
 			(Value1, Value2) = (Value2, Value1);
 		}
 
-		public static void BubbleSort(int[] ArrayToSort, ref long Counter)
+		/// <summary>
+		/// Пузырьковая сортировка без какой бы то ни было оптимизации
+		/// </summary>
+		/// <param name="ArrayToSort">Обрабатываемый массив</param>
+		/// <param name="SwapCounter">Счетчик замен местами элементов массива</param>
+		/// <param name="CycleCounter">Счетчик проходов по каждому циклу</param>
+		public static void BubbleSortSimple(int[] ArrayToSort, out long SwapCounter, out long CycleCounter)
 		{
-			for (int i = 0; i < ArrayToSort.Length; i++)
+			int[] temp = (int[])ArrayToSort.Clone();
+			SwapCounter = 0;
+			CycleCounter = 0;
+			for (int i = 0; i < temp.Length; i++)
 			{
-				for (int j = i + 1; j < ArrayToSort.Length; j++)
+				CycleCounter++;
+				for (int j = 0; j < temp.Length; j++)
 				{
-					if (ArrayToSort[i] > ArrayToSort[j])
-					{
-						Swap(ref ArrayToSort[i], ref ArrayToSort[j], ref Counter);
-					}
+					CycleCounter++;
+					if (temp[i] < temp[j])
+						Swap(ref temp[i], ref temp[j], ref SwapCounter);
 				}
 			}
 		}
 
 		/// <summary>
-		/// Неправильная реализация. Что-то я сделал не так...
+		/// Слегка улучшенная версия пузырьковой сортировки
+		/// </summary>
+		/// <param name="ArrayToSort">Обрабатываемый массив</param>
+		/// <param name="SwapCounter">Счетчик замен местами элементов массива</param>
+		/// <param name="CycleCounter">Счетчик проходов по каждому циклу</param>
+		public static void BubbleSort(int[] ArrayToSort, out long SwapCounter, out long CycleCounter)
+		{
+			SwapCounter = 0;
+			CycleCounter = 0;
+			int[] temp = (int[])ArrayToSort.Clone();
+			for (int i = 0; i < temp.Length; i++)
+			{
+				CycleCounter++;
+				for (int j = i + 1; j < temp.Length; j++)
+				{
+					CycleCounter++;
+					if (temp[i] > temp[j])
+						Swap(ref temp[i], ref temp[j], ref SwapCounter);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Первый вариант "шейкерной" сортировки. 
 		/// </summary>
 		/// <param name="ArrayToSort"></param>
-		/// <param name="Counter"></param>
-		public static void ShakerSort(int[] ArrayToSort, ref long Counter)
+		/// <param name="SwapCounter"></param>
+		/// <param name="CycleCounter"></param>
+		public static void ShakerSort1(int[] ArrayToSort, out long SwapCounter, out long CycleCounter)
 		{
-			for (int arCount = 0; arCount < ArrayToSort.Length/2; arCount++)
+			SwapCounter = 0;
+			CycleCounter = 0;
+			int[] temp = (int[])ArrayToSort.Clone();
+			for (int arCount = 0; arCount < temp.Length / 2; arCount++)
 			{
+				CycleCounter++;
 				bool flag = false;
-				for (int front = arCount; front < ArrayToSort.Length-arCount-1; front++)
+				for (int front = arCount; front < temp.Length - arCount - 1; front++)
 				{
-					if (ArrayToSort[front]> ArrayToSort[front+1])
+					CycleCounter++;
+					if (temp[front] > temp[front + 1])
 					{
-						Swap(ref ArrayToSort[front], ref ArrayToSort[front + 1], ref Counter);
+						Swap(ref temp[front], ref temp[front + 1], ref SwapCounter);
 						flag = true;
 					}
 
-					for (int back = ArrayToSort.Length - 2 - arCount; back > front; back--)
+					for (int back = temp.Length - 2 - arCount; back > front; back--)
 					{
-						if (ArrayToSort[back-1]> ArrayToSort[back])
+						CycleCounter++;
+						if (temp[back - 1] > temp[back])
 						{
-							Swap(ref ArrayToSort[back - 1], ref ArrayToSort[back], ref Counter);
+							Swap(ref temp[back - 1], ref temp[back], ref SwapCounter);
 							flag = true;
 						}
 					}
 					if (!flag)
 						break;
 				}
-				
+
+			}
+		}
+
+		/// <summary>
+		/// Попытка переработать "шейкерную" сортировку.
+		/// </summary>
+		/// <param name="ArrayToSort">Обрабатываемый массив</param>
+		/// <param name="SwapCounter">Счетчик замен местами элементов массива</param>
+		/// <param name="CycleCounter">Счетчик проходов по каждому циклу</param>
+		public static void ShakerSort2(int[] ArrayToSort, out long SwapCounter, out long CycleCounter)
+		{
+			SwapCounter = 0;
+			CycleCounter = 0;
+			int[] temp = (int[])ArrayToSort.Clone();
+			int left = 0;
+			int right = temp.Length - 1;
+			int last = right;//temp.Length - 1;
+
+			do
+			{
+				CycleCounter++;
+				for (int i = left; i < right; i++)
+				{
+					CycleCounter++;
+					if (temp[i] > temp[i + 1])
+						Swap(ref temp[i], ref temp[i + 1], ref SwapCounter);
+				}
+				right--;
+				for (int i = right; i > left; i--)
+				{
+					CycleCounter++;
+					if (temp[i] < temp[i + 1])
+						Swap(ref temp[i], ref temp[i + 1], ref SwapCounter);
+				}
+			} while (left < right);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="ArrayToSort"></param>
+		/// <param name="SwapCounter"></param>
+		/// <param name="CycleCounter"></param>
+		public static void InsSort1(int[] ArrayToSort, out long SwapCounter, out long CycleCounter)
+		{
+			SwapCounter = 0;
+			CycleCounter = 0;
+			int[] temp = (int[])ArrayToSort.Clone();
+			for (int i = 0; i < temp.Length; i++)
+			{
+				CycleCounter++;
+				int key = temp[i];
+				int j = i;
+				while ((j > 1) && (temp[j - 1] > key))
+				{
+					Swap(ref temp[j - 1], ref temp[j], ref SwapCounter);
+					j--;
+				}
+				temp[j] = key;
 			}
 		}
 	}
