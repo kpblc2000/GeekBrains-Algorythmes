e327322aa6f5da7376a6965ad4de9ce7d81b7557
Alexey Kulik <kpblc2000@gmail.com>
2020-08-21 19:56:55 +0300

    Удаление ненужных кусков кода.

--- Lesson05/Task2/Program.cs
+++ Lesson05/Task2/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 /// <summary>
 /// Алексей Кулик kpblc2000@yandex.ru
@@ -9,7 +8,6 @@ using System.Linq;
 
 namespace Task2
 {
-
 	class StringStack
 	{
 		private string[] _array;
@@ -48,270 +46,42 @@ namespace Task2
 		public int Length { get { return _pos + 1; } }
 	}
 
-	class CharStack
-	{
-		// private int _size; // Количество элементо в стеке
-		private char[] _array; // Собственно содержимое стека. Сделаю только под символы
-		private int _pos; // Текущее количество элементов в стеке
-
-		public CharStack(int Size)
-		{
-			_array = new char[Size];
-			_pos = -1;
-		}
-
-		/// <summary>
-		/// Загнать символ в стек
-		/// </summary>
-		/// <param name="Symbol"></param>
-		/// <returns></returns>
-		public void Push(char Symbol)
-		{
-			_pos += 1;
-			_array[_pos] = Symbol;
-		}
-
-		/// <summary>
-		/// Получить символ из стека, удалив его
-		/// </summary>
-		/// <returns></returns>
-		public char Pop()
-		{
-			if (_pos >= 0)
-			{
-				char res = _array[_pos];
-				_array[_pos] = '\0';
-				_pos -= 1;
-				return res;
-			}
-			else return '\0';
-		}
-
-		public int Length { get { return _pos; } }
-
-		public bool IsEmpty { get { return _pos < 0; } }
-
-	}
-
-
 	class Program
 	{
 		static void Main()
 		{
 			// Предполагается, что строка все же более-менее нормально сформирована
-			string baseString = " 198 + 2 * 36 - 5";
-			CharStack oper = new CharStack(baseString.Length);
-			string digVal = "";
+			// Для чисел с несколькими разрядами надо писать дополнительный функционал, определяющий их границы
+			string baseString = "( 1 + 2 ) * (3 - 5 * 8)";
+			string resString = "";
 
-			char[] operations = new char[] { '+', '-', '*', '/' };
-
-			string res = "";
-
-			/*
-( 192 + ( 8 - 4 ) ) * ( 36 - 5 )
-Символ	Операция	Стек	Выходная строка
-(	поместить в стек	(	
-192 	добавить к выходной строке	(	192
-+	поместить в стек	(, +	192
-	добавить к выходной строке	(, +	192 ,
-(	поместить в стек	(, +, (	192 ,
-8 	добавить к выходной строке	(, +, (	192 , , 8
--	поместить в стек	(, +, (, -	192 , , 8
-4 	добавить к выходной строке	(, +, (, -	192 , , 8 , 4
-)	1) присоединить содержимое стека до скобки в обратном порядке к выходной строке; 2) удалить скобку из стека.	(, +	192 , , 8 , 4 , -
-	добавить к выходной строке	(, +	192 , , 8 , 4 , -,
-)	1) присоединить содержимое стека до скобки в обратном порядке к выходной строке; 2) удалить скобку из стека.		192 , , 8 , 4 , -, , +
-	добавить к выходной строке		192 , , 8 , 4 , -, , +,
-*	поместить в стек	*	192 , , 8 , 4 , -, , +,
-	добавить к выходной строке	*	192 , , 8 , 4 , -, , +, ,
-(	поместить в стек	*, (	192 , , 8 , 4 , -, , +, ,
-36 	добавить к выходной строке	*, (	192 , , 8 , 4 , -, , +, , , 36
--	поместить в стек	*, (, -	192 , , 8 , 4 , -, , +, , , 36
-5 	добавить к выходной строке	*, (, -	192 , , 8 , 4 , -, , +, , , 36 , 5
-)	1) присоединить содержимое стека до скобки в обратном порядке к выходной строке; 2) удалить скобку из стека.	*	192 , , 8 , 4 , -, , +, , , 36 , 5 , -
-			 */
-
-			/*
-( 192 + 8 ) * ( 36 - 5 )
-Символ	Операция	Стек	Выходная строка
-(	поместить в стек	(	
-192 	добавить к выходной строке	(	192
-+	поместить в стек	(, +	192
-8 	добавить к выходной строке	(, +	192 , 8
-)	1) присоединить содержимое стека до скобки в обратном порядке к выходной строке; 2) удалить скобку из стека.		192 , 8 , +
-	добавить к выходной строке		192 , 8 , +,
-*	поместить в стек	*	192 , 8 , +,
-	добавить к выходной строке	*	192 , 8 , +, ,
-(	поместить в стек	*, (	192 , 8 , +, ,
-36 	добавить к выходной строке	*, (	192 , 8 , +, , , 36
--	поместить в стек	*, (, -	192 , 8 , +, , , 36
-5 	добавить к выходной строке	*, (, -	192 , 8 , +, , , 36 , 5
-)	1) присоединить содержимое стека до скобки в обратном порядке к выходной строке; 2) удалить скобку из стека.	*	192 , 8 , +, , , 36 , 5 , -
-			 */
-
+			StringStack st = new StringStack(baseString.Length);
 			for (int i = 0; i < baseString.Length; i++)
 			{
 				char curSym = baseString[i];
+				string cur = curSym.ToString();
 				if (char.IsDigit(curSym))
 				{
-					digVal += curSym.ToString();
+					resString += " " + cur;
 				}
-				else if (operations.Contains(curSym))
+				else if (cur == "+" || cur == "-" || cur == "*" || cur == "/" || cur == "(")
 				{
-					oper.Push(curSym);
+					st.Push(cur);
 				}
-				else
+				else if (cur == ")")
 				{
-					switch (curSym)
+					while ((cur = st.Pop()) != null)
 					{
-						case '(':
-							oper.Push(curSym);
-							break;
-						case ')':
-							// Надо вытаскивать все, пока не получим '('
-							char opSym;
-							while ((opSym = oper.Pop()) != '\0')
-							{
-								if (opSym != '(')
-								{
-									res += opSym.ToString() + digVal;
-								}
-								else
-								{ break; }
-							}
-							break;
-						default:
-							break;
+						if (cur != "(")
+						{
+							resString += " " + cur;
+						}
 					}
 				}
 			}
 
-			////string tempValue = "";
-			////			bool flag = false;
-			////char oper;
-
-			//for (int i = 0; i < baseString.Length; i++)
-			//{
-			//	char curSym = baseString[i];
-
-			//	if (char.IsDigit(curSym))
-			//	{
-			//		// Это число
-			//		switch (st.Length)
-			//		{
-			//			case 0:
-			//				// В стеке вообще ничего нет
-			//				st.Push(curSym.ToString());
-			//				break;
-			//			case 1:
-			//				// В стеке что-то есть. Получаем, проверяем.
-			//				string val = st.Pop();
-			//				if (char.IsDigit(val[0]))
-			//				{
-			//					// Это у нас число
-			//					val += curSym.ToString();
-			//					st.Push(val);
-			//				}
-			//				break;
-			//			default:
-			//				// В стеке 2 значения
-
-			//				break;
-			//		}
-			//	}
-			//	else if (operations.Contains(curSym))
-			//	{
-			//		// Какая-то операция
-			//	}
-
-			//	if (operations.Contains(curSym))
-			//	//(operations.Contains(curSym) && st.Length != 0)
-			//	{
-			//		switch (st.Length)
-			//		{
-			//			case 1:
-			//				st.Push(curSym.ToString());
-			//				break;
-			//			case 2:
-			//				string val = st.Pop();
-			//				res = res + " " + st.Pop() + " " + val + " ";
-			//				st.Push(curSym.ToString());
-			//				break;
-			//			default:
-			//				st.Push(curSym.ToString());
-			//				//st.Push(curSym.ToString());
-			//				//st.Pop();
-			//				break;
-			//		}
-			//	}
-			//	else if (char.IsDigit(curSym))
-			//	{
-			//		if (st.Length < 2)
-			//		{
-			//			string val = st.Pop();
-
-			//			if (val == null)
-			//			{
-			//				st.Push(curSym.ToString());
-			//			}
-			//			else if (char.IsDigit(val[0]))
-			//			{
-			//				val += curSym.ToString();
-			//				st.Push(val);
-			//			}
-			//			else
-			//			{
-			//				st.Push(val);
-			//				st.Push(curSym.ToString());
-			//			}
-
-			//		}
-			//		else
-			//		{
-			//			if (res.Length == 0)
-			//			{
-			//				res = st.Pop() + " " + st.Pop() + " " + curSym.ToString();
-			//			}
-			//			else
-			//			{
-			//				string val = st.Pop();
-			//				res = res + " " + st.Pop() + val;
-			//			}
-			//			//res = res + (res.Length == 0 ? "" : " ") + st.Pop() + " " + st.Pop() + " " + curSym.ToString();
-			//		}
-			//	}
-			//}
-			/*
-
-			//if (char.IsDigit(curSym) || operations.Contains(curSym))
-			//{
-			//	if (curSym == ' ')
-			//	{
-			//		// Пробел, чего-то от чего-то отделено
-			//		switch (st.Length)
-			//		{
-			//			case 0:
-			//			case 1:
-			//				st.Push(tempValue);
-			//				break;
-			//			default:
-			//				res = st.Pop() + st.Pop() + tempValue + res;
-			//				break;
-			//		}
-			//		tempValue = "";
-			//	}
-			//	else
-			//	{
-			//		tempValue = tempValue + curSym.ToString();
-			//	}
-			//}
-		}
-			*/
-			// postfix  : 1 12 + 3 * 5 -
-			// prefix   : + 1 12 * 3 - 5
-
 			Console.WriteLine($"Base string : {baseString}");
-			Console.WriteLine($"Potfix string : {res}");
+			Console.WriteLine($"Potfix string : {resString}");
 
 			Console.WriteLine("Нажмите любую клавишу");
 			Console.ReadKey();
