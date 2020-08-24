f9b4e8dc234dabc865e8918413784aa20a9caa30
Alexey Kulik <kpblc2000@gmail.com>
2020-08-24 08:31:57 +0300

    Задача подсчета суммы кодов символов из ком.строки

---
+++ Task01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// Алексей Кулик kpblc2000@yandex.ru
+/// Алгоритмы. Урок 6, задача 1
+/// Реализовать простейшую хэш-функцию. На вход функции подается строка, на выходе сумма кодов символов
+/// </summary>
+
+namespace Task01
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("\nВведите строку для обработки : ");
+			string userString = Console.ReadLine();
+			int res = 0;
+			for (int i = 0; i < userString.Length; i++)
+			{
+				res += (int)userString[i];
+			}
+
+			Console.WriteLine($"Общая сумма кодов символов {res}");
+			Console.WriteLine("Нажмите любую клавишу");
+			Console.ReadKey();
+		}
+	}
+}
